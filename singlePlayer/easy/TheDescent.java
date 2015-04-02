dea7cd3016868160c00ae957350e6881c1c2bf99
Manuel Campomanes <manik971@users.noreply.github.com>
2015-04-02 12:42:14 +0200

    Update TheDescent.java

--- singlePlayer/easy/TheDescent.java
+++ singlePlayer/easy/TheDescent.java
@@ -1,34 +1,42 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 
 /**
  * The Descent
  **/
 class Player {
 
+    /**
+     * Main
+     */
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
 
         // game loop
         while (true) {
-            int SX = in.nextInt();
-            int SY = in.nextInt();
-            int MH_max_coord = 0;
-            int MH_max = 0;
+            // starship X position
+            int starshipX = in.nextInt();
+            // starship Y position
+            int starshipY = in.nextInt();
+            // higher mountain position
+            int mtnMaxPos = 0;
+            // higher mountain height
+            int mtnMaxHeight = 0;
             String action = "HOLD";
             for (int i = 0; i < 8; i++) {
-                // represents the height of one mountain, from 9 to 0. Mountain heights are provided from left to right.
-                int MH = in.nextInt();
-                if(MH > MH_max){
-                    MH_max = MH;
-                    MH_max_coord = i;
+                // represents the height of one mountain, from 9 to 0. Mountain
+                // heights are provided from left to right.
+                int currentMtnHeight = in.nextInt();
+                if (currentMtnHeight > mtnMaxHeight) {
+                    mtnMaxHeight = currentMtnHeight;
+                    mtnMaxPos = i;
                 }
             }
-            if(SX == MH_max_coord){
+            if (starshipX == mtnMaxPos) {
                 action = "FIRE";
             }
-            // FIRE (ship is firing its phase cannons) or HOLD (ship is not firing)
-            System.out.println(action); 
+            // FIRE (ship is firing its phase cannons)
+            // HOLD (ship is not firing)
+            System.out.println(action);
         }
     }
 }
