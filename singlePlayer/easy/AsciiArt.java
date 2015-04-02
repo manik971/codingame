550b015f1fe3633231ffb83bb55e685d9cd3f4a0
Manuel Campomanes <manik971@users.noreply.github.com>
2015-04-02 12:12:31 +0200

    Update AsciiArt.java

--- singlePlayer/easy/AsciiArt.java
+++ singlePlayer/easy/AsciiArt.java
@@ -1,49 +1,59 @@
-import java.util.*;
-import java.io.*;
-import java.math.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * ASCII Art
  **/
 class Solution {
 
+    /**
+     * Main
+     **/
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
-        int L = in.nextInt(); // the width of letters represented in ASCII art
+        // the width of letters represented in ASCII art
+        int width = in.nextInt();
         in.nextLine();
-        int H = in.nextInt(); // the height of letters represented in ASCII art
+        // the height of letters represented in ASCII art
+        int height = in.nextInt();
         in.nextLine();
-        String text = in.nextLine(); // the entry text
-        //change letter to index number in asciiLetters tab
-        List<Integer> ascii_letters_list = getIndexAscii(text.toUpperCase());
-        //print ASCII letters
-        for (int i = 0; i < H; i++) {
-            String ROW = in.nextLine();
+        // the entry text
+        String text = in.nextLine();
+        // change letter to index number in asciiLetters tab
+        List<Integer> asciiLettersList = getIndexAscii(text.toUpperCase());
+        // print ASCII letters
+        for (int i = 0; i < height; i++) {
+            String row = in.nextLine();
             String print = "";
-            for(Integer ascii_letter : ascii_letters_list){
-                print += ROW.substring(ascii_letter * L, ascii_letter * L + L);
+            for (Integer asciiLetter : asciiLettersList) {
+                print += row.substring(asciiLetter * width, asciiLetter * width + width);
             }
             System.out.println(print);
         }
     }
-    
+
     /**
+     * Get index letters in ASCII table
      * 
-     **/
-    public static List<Integer> getIndexAscii(String text_){
-        List<Integer> ascii_letters = new ArrayList<Integer>(text_.length());
-        //index A in ascii table
-        int index_A = (int) 'A';
-        for(char letter : text_.toCharArray()){
-            int index_letter = (int) letter;
+     * @param text_
+     *            entry text
+     * @return list of letters index
+     */
+    public static List<Integer> getIndexAscii(String text_) {
+        List<Integer> asciiLetters = new ArrayList<Integer>(text_.length());
+        // index A in ascii table
+        int indexA = 'A';
+        for (char letter : text_.toCharArray()) {
+            int indexLetter = letter;
             // index of symbol '?'
-            int index_asciiArt = 26;
+            int indexAsciiArt = 26;
             // if in the intervals [A-Z]
-            if(index_letter >= 65 && index_letter <= 90){
-                index_asciiArt = index_letter - index_A;
+            if (indexLetter >= 65 && indexLetter <= 90) {
+                indexAsciiArt = indexLetter - indexA;
             }
-            ascii_letters.add(index_asciiArt);
+            asciiLetters.add(indexAsciiArt);
         }
-        return ascii_letters;
+        return asciiLetters;
     }
 }
