236dbae6832ce806fce4bc1526cd1b3621e7f746
Manuel Campomanes <manik971@users.noreply.github.com>
2015-04-02 12:31:27 +0200

    Update Temperatures.java

--- singlePlayer/easy/Temperatures.java
+++ singlePlayer/easy/Temperatures.java
@@ -1,29 +1,31 @@
-import java.util.*;
-import java.io.*;
-import java.math.*;
+import java.util.Scanner;
 
 /**
  * Temperatures
  **/
 class Solution {
 
+    /**
+     * Main
+     **/
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         int result = 5526;
-        int N = in.nextInt(); // the number of temperatures to analyse
-        if(N > 0){
+        // the number of temperatures to analyse
+        int nbTemp = in.nextInt();
+        if (nbTemp > 0) {
             in.nextLine();
             // the N temperatures expressed as integers ranging from -273 to 5526
-            String [] temperatures = in.nextLine().split(" "); 
-            for(int i=0;i<N;i++){
-                int currentTemp = Integer.parseInt(temperatures[i]);
+            String[] temps = in.nextLine().split(" ");
+            for (int i = 0; i < nbTemp; i++) {
+                int currentTemp = Integer.parseInt(temps[i]);
                 // save the temperature closest to 0
-                if(Math.abs(currentTemp) < Math.abs(result)){
+                if (Math.abs(currentTemp) < Math.abs(result)) {
                     result = currentTemp;
                 }
                 // if two numbers are equally close to zero, keep the positive integer
-                else if(Math.abs(currentTemp) == Math.abs(result)){
-                    if(currentTemp > 0){
+                else if (Math.abs(currentTemp) == Math.abs(result)) {
+                    if (currentTemp > 0) {
                         result = currentTemp;
                     }
                 }
