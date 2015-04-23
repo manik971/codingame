0cc3ca401bf70ab64cadcd4b4c1bc66b5dad166f
manik971 <campomanes.manuel@gmail.com>
2015-04-23 15:48:05 +0200

    checkstyle

--- singlePlayer/easy/MimeType.java
+++ singlePlayer/easy/MimeType.java
@@ -3,39 +3,53 @@ import java.io.*;
 import java.math.*;
 
 /**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
+ * MIME Type
  **/
 class Solution {
 
+    /**
+     * Main
+     **/
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
-        int N = in.nextInt(); // Number of elements which make up the association table.
+        // Number of elements which make up the association table
+        int nbAssociation = in.nextInt();
         in.nextLine();
-        int Q = in.nextInt(); // Number Q of file names to be analyzed.
+        // Number of file names to be analyzed
+        int nbFiles = in.nextInt();
         in.nextLine();
-        
+
         Map<String, String> tableExt = new HashMap<String, String>();
-        for (int i = 0; i < N; i++) {
-            String EXT = in.next(); // file extension
-            String MT = in.next(); // MIME type.
-            tableExt.put(EXT.toLowerCase(), MT);
+        for (int i = 0; i < nbAssociation; i++) {
+            // file extension
+            String fileExt = in.next();
+            // MIME type
+            String mimeType = in.next();
+            tableExt.put(fileExt.toLowerCase(), mimeType);
             in.nextLine();
         }
-        for (int i = 0; i < Q; i++) {
-            String FNAME = in.nextLine(); // One file name per line.
-            String ext = getFileExtension(FNAME.toLowerCase());
-            String sortie = "UNKNOWN";
-            if(ext != null && tableExt.containsKey(ext)){
-                sortie = tableExt.get(ext);
+        for (int i = 0; i < nbFiles; i++) {
+            // One file name per line
+            String fileName = in.nextLine();
+            String ext = getFileExtension(fileName.toLowerCase());
+            String result = "UNKNOWN";
+            if (ext != null && tableExt.containsKey(ext)) {
+                result = tableExt.get(ext);
             }
-            System.out.println(sortie);
+            System.out.println(result);
         }
     }
-    
-    public static String getFileExtension(String fileName_){
+
+    /**
+     * Get the file extension
+     * 
+     * @param fileName_
+     *            Name of the file
+     * @return the extension
+     **/
+    public static String getFileExtension(String fileName_) {
         String ext = null;
-        if(fileName_.lastIndexOf(".") >= 0 && fileName_.lastIndexOf(".") < fileName_.length() - 1){
+        if (fileName_.lastIndexOf(".") >= 0 && fileName_.lastIndexOf(".") < fileName_.length() - 1) {
             ext = fileName_.substring(fileName_.lastIndexOf(".") + 1);
         }
         return ext;
