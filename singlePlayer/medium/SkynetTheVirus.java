c6155613d9515bce1d35813324ddc7b1e47444af
manik971 <campomanes.manuel@gmail.com>
2015-04-27 15:14:09 +0200

    update

--- singlePlayer/medium/SkynetTheVirus.java
+++ singlePlayer/medium/SkynetTheVirus.java
@@ -1,196 +1,252 @@
-import java.util.*;
-import java.io.*;
-import java.math.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Stack;
 import java.util.concurrent.atomic.AtomicInteger;
- 
+
+/**
+ * Skynet: the Virus
+ */
 class Player {
- 
-	public class Link {
- 
-		private int m_n1;
-		private int m_n2;
- 
-		public Link(int n1, int n2) {
-			m_n1 = n1;
-			m_n2 = n2;
-		}
- 
-		public void severs() {
-			System.out.println(this.toString());
-		}
-		
-		public String toString() {
-			return m_n1 + " " + m_n2;
-		}
- 
-		public boolean equals(Object o) {
-			Link otherLink = (Link) o;
-			return ((m_n1 == otherLink.m_n1 && m_n2 == otherLink.m_n2) || (m_n2 == otherLink.m_n1 && m_n1 == otherLink.m_n2));
-		}
-		
-		public int getNodeA(){
-		    return this.m_n1;
-		}
-		
-		public int getNodeB(){
-		    return this.m_n2;
-		}
-	}
-	
-	public class Subnet {
-	    
-	    private int gateParent;
-	    private List<Integer> nodes;
-	    private Stack<Link> input_output;
-	    private int status;
-	    
-	    public Subnet(int gateParent_){
-	        this.gateParent = gateParent_;
-	        this.nodes = new ArrayList<Integer>();
-	        this.input_output = new Stack<Link>();
-	        this.status = 2; // 0:closed 1:in progress 2:open
-	    }
-	    
-	    public Link getLink(List<Link> links_, List<Integer> gateways_){
-	        if(this.status == 2){
-	            this.searchInputOutput(links_, gateways_);
-	            this.status = 1;
-	        }
-	        Link link = this.input_output.pop();
-	        if(this.input_output.empty()){
-	            this.status = 0;
-	        }
-	        return link;
-	    }
-	    
-	    public void searchInputOutput(List<Link> links_, List<Integer> gateways_){
-	        for(Link link : links_){
-	            if(this.nodes.contains(link.getNodeA()) || this.nodes.contains(link.getNodeB())){
-	                if(!this.nodes.contains(link.getNodeA()) || !this.nodes.contains(link.getNodeB())){
-	                    if(!gateways_.contains(link.getNodeA()) && !gateways_.contains(link.getNodeB())){
-	                        this.input_output.push(link);
-	                    }
-	                }
-	            }
-	        }
-	    }
-	    
-	    public int getStatus(){
-	        return this.status;
-	    }
-	    
-	    public void print(){
-	        System.err.println("--SUBNET--");
-	        for(Integer node : this.nodes){
-	            System.err.print(node+" ");
-	        }
-	        System.err.println(" ");
-	    }
-	    
-	    public void addNode(Integer node_){
-	        this.nodes.add(node_);
-	    }
-	    
-	    public int getNbNodes(){
-	        return this.nodes.size();
-	    }
-	}
- 
-	public boolean blockDirectLinkToGate(int agentNode, List<Integer> gateways,
-			List<Link> links, List<AtomicInteger> nbConnections) {
-			    
-	    boolean ret = false;
- 
-		for (Integer gateway : gateways) {
-			Link linkToTest = new Link(agentNode, gateway);
-			int linkIndex = links.indexOf(linkToTest);
-			if (linkIndex >= 0) {
-				linkToTest.severs();
-				links.remove(linkIndex);
-				nbConnections.get(gateway).decrementAndGet();
-				nbConnections.get(agentNode).decrementAndGet();
-				ret = true;
-				break;
-			}
-		}
-		return ret;
-	}
-	
-	public static void main(String args[]) {
-		Scanner in = new Scanner(System.in);
-		Player player = new Player();
-		// Read init information from standard input, if any
-		int nbNodes = in.nextInt();
-		int nbLinks = in.nextInt();
-		int nbExits = in.nextInt();
- 
-		List<Link> links = new ArrayList<Link>(nbLinks);
-		List<Integer> gateways = new ArrayList<Integer>(nbExits);
-		List<AtomicInteger> nbConnections = new ArrayList<AtomicInteger>(nbNodes);
-		Map<Integer, Subnet> subnets = new HashMap<Integer, Subnet>();
-		
-		//init connection counter
-		for (int i = 0; i < nbNodes; i++) {
-		    nbConnections.add(i, new AtomicInteger(0));
-		}
-		
-		//init Links
-		for (int i = 0; i < nbLinks; i++) {
-			int nodeA = in.nextInt();
-			int nodeB = in.nextInt();
-			links.add(player.new Link(nodeA, nodeB));
-			nbConnections.get(nodeA).incrementAndGet();
-			nbConnections.get(nodeB).incrementAndGet();
-		}
- 
-        //init gateways
-		for (int j = 0; j < nbExits; j++) {
-		    int gate = in.nextInt();
-			gateways.add(gate);
-			subnets.put(gate, player.new Subnet(gate));
-		}
-		
-		//init subnets
-		for(Link link : links){
-		    if(gateways.contains(link.getNodeA()) && nbConnections.get(link.getNodeB()).intValue() == 3){
-		        subnets.get(link.getNodeA()).addNode(link.getNodeB());
-		    }
-		    else if(gateways.contains(link.getNodeB()) && nbConnections.get(link.getNodeA()).intValue() == 3){
-		        subnets.get(link.getNodeB()).addNode(link.getNodeA());
-		    }
-		}
-		
-		while (true) {
-			// Read information from standard input
-			int agentNode = in.nextInt();
- 
-			// Compute logic here
-			if (!player.blockDirectLinkToGate(agentNode, gateways, links, nbConnections)) {
-				// Block subnets
-				Subnet subnetToBlock = null;
-				int nbMax = 0;
-				for(Map.Entry<Integer, Subnet> subnetEntry : subnets.entrySet()){
-				    if(subnetEntry.getValue().getStatus() == 1){
-				        subnetToBlock = subnetEntry.getValue();
-				        break;
-				    }
-				    else if(subnetEntry.getValue().getStatus() == 2 && subnetEntry.getValue().getNbNodes() > nbMax){
-				        subnetToBlock = subnetEntry.getValue();
-				        nbMax = subnetEntry.getValue().getNbNodes();
-				    }
-				}
-				if(subnetToBlock != null && subnetToBlock.getNbNodes() > 0){
-				    Link linkToBlock = subnetToBlock.getLink(links, gateways);
-    				linkToBlock.severs();
-    				links.remove(links.indexOf(linkToBlock));
-    				nbConnections.get(linkToBlock.getNodeA()).decrementAndGet();
-    				nbConnections.get(linkToBlock.getNodeB()).decrementAndGet();
-				}
-				else{
-				    links.get(0).severs();
-				    links.remove(0);
-				}
-			}
-		}
-	}
+
+    /**
+     * Link between two nodes
+     */
+    public class Link {
+
+        private final int _nodeA;
+        private final int _nodeB;
+
+        public Link(int nodeA_, int nodeB_) {
+            _nodeA = nodeA_;
+            _nodeB = nodeB_;
+        }
+
+        /**
+         * Sever a link
+         */
+        public void sever() {
+            System.out.println(this.toString());
+        }
+
+        @Override
+        public String toString() {
+            return _nodeA + " " + _nodeB;
+        }
+
+        @Override
+        public boolean equals(Object other_) {
+            Link otherLink = (Link) other_;
+            return ((_nodeA == otherLink._nodeA && _nodeB == otherLink._nodeB) || (_nodeB == otherLink._nodeA && _nodeA == otherLink._nodeB));
+        }
+
+        public int getNodeA() {
+            return this._nodeA;
+        }
+
+        public int getNodeB() {
+            return this._nodeB;
+        }
+    }
+
+    /**
+     * Subnetwork : List of nodes connected only to a gate or to each other
+     */
+    public class Subnet {
+
+        // nodes in the subnet
+        private final List<Integer> _nodes;
+        // input and output links
+        private final Stack<Link> _inputOutput;
+        // subnet open status
+        // 0:closed 1:closing 2:open
+        private int _status;
+
+        public Subnet() {
+            this._nodes = new ArrayList<Integer>();
+            this._inputOutput = new Stack<Link>();
+            this._status = 2;
+        }
+
+        /**
+         * Get a input or a output link to close this subnet
+         * 
+         * @param links_
+         *            list of links
+         * @param gateways_
+         *            list of gateways
+         * @return a input or output link
+         */
+        public Link getInputOutputLink(List<Link> links_, List<Integer> gateways_) {
+            // first access to this subnet
+            if (this._status == 2) {
+                this.initInputOuputLinks(links_, gateways_);
+                this._status = 1;
+            }
+            Link link = this._inputOutput.pop();
+            // close the subnet if there is no more input output
+            if (this._inputOutput.empty()) {
+                this._status = 0;
+            }
+            return link;
+        }
+
+        /**
+         * Initialize the subnet input/output links
+         * 
+         * @param links_
+         *            list of links
+         * @param gateways_
+         *            list of gateways
+         */
+        public void initInputOuputLinks(List<Link> links_, List<Integer> gateways_) {
+            for (Link link : links_) {
+                if (this._nodes.contains(link.getNodeA()) || this._nodes.contains(link.getNodeB())) {
+                    if (!this._nodes.contains(link.getNodeA()) || !this._nodes.contains(link.getNodeB())) {
+                        if (!gateways_.contains(link.getNodeA()) && !gateways_.contains(link.getNodeB())) {
+                            this._inputOutput.push(link);
+                        }
+                    }
+                }
+            }
+        }
+
+        /**
+         * Add a node to the subnet
+         * 
+         * @param node_
+         *            a node
+         */
+        public void addNode(Integer node_) {
+            this._nodes.add(node_);
+        }
+
+        public int getNbNodes() {
+            return this._nodes.size();
+        }
+
+        public int getStatus() {
+            return this._status;
+        }
+    }
+
+    /**
+     * Main
+     */
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        Player player = new Player();
+
+        // number of nodes
+        int nbNodes = in.nextInt();
+        // number of links
+        int nbLinks = in.nextInt();
+        // number of exit gateways
+        int nbGateways = in.nextInt();
+        // links
+        List<Link> links = new ArrayList<Link>(nbLinks);
+        // exit gateways
+        List<Integer> gateways = new ArrayList<Integer>(nbGateways);
+        // number of connections for each node
+        List<AtomicInteger> nbConnections = new ArrayList<AtomicInteger>(nbNodes);
+        // one subnet for each gateway
+        Map<Integer, Subnet> subnets = new HashMap<Integer, Subnet>();
+
+        // initialize connection counters
+        for (int i = 0; i < nbNodes; i++) {
+            nbConnections.add(i, new AtomicInteger(0));
+        }
+
+        // initialize links
+        for (int i = 0; i < nbLinks; i++) {
+            int nodeA = in.nextInt();
+            int nodeB = in.nextInt();
+            links.add(player.new Link(nodeA, nodeB));
+            // update connection counters
+            nbConnections.get(nodeA).incrementAndGet();
+            nbConnections.get(nodeB).incrementAndGet();
+        }
+
+        // initialize exit gateways
+        for (int j = 0; j < nbGateways; j++) {
+            int gate = in.nextInt();
+            gateways.add(gate);
+            subnets.put(gate, player.new Subnet());
+        }
+
+        // initialize subnets
+        // a node in the subnet can't have more than 3 connections
+        for (Link link : links) {
+            if (gateways.contains(link.getNodeA()) && nbConnections.get(link.getNodeB()).intValue() <= 3) {
+                subnets.get(link.getNodeA()).addNode(link.getNodeB());
+            }
+            else if (gateways.contains(link.getNodeB()) && nbConnections.get(link.getNodeA()).intValue() <= 3) {
+                subnets.get(link.getNodeB()).addNode(link.getNodeA());
+            }
+        }
+
+        // game loop
+        while (true) {
+            int skynetPosition = in.nextInt();
+
+            // block direct link to gateway
+            boolean action = false;
+            for (Integer gateway : gateways) {
+                Link linkToTest = player.new Link(skynetPosition, gateway);
+                int linkIndex = links.indexOf(linkToTest);
+                if (linkIndex >= 0) {
+                    severLink(links, nbConnections, linkIndex);
+                    action = true;
+                    break;
+                }
+            }
+
+            // no incoming danger, block the best link
+            if (!action) {
+                // try to find a subnet to block
+                Subnet subnetToBlock = null;
+                int nbMax = 0;
+                for (Map.Entry<Integer, Subnet> subnetEntry : subnets.entrySet()) {
+                    // select in priority a subnet in closing progress
+                    if (subnetEntry.getValue().getStatus() == 1) {
+                        subnetToBlock = subnetEntry.getValue();
+                        break;
+                    }
+                    // otherwise select the largest open subnet
+                    else if (subnetEntry.getValue().getStatus() == 2 && subnetEntry.getValue().getNbNodes() > nbMax) {
+                        subnetToBlock = subnetEntry.getValue();
+                        nbMax = subnetEntry.getValue().getNbNodes();
+                    }
+                }
+                // a subnet can be blocked
+                if (subnetToBlock != null && subnetToBlock.getNbNodes() > 0) {
+                    Link linkToBlock = subnetToBlock.getInputOutputLink(links, gateways);
+                    severLink(links, nbConnections, links.indexOf(linkToBlock));
+                }
+                // otherwise block a random link
+                else {
+                    severLink(links, nbConnections, 0);
+                }
+            }
+        }
+    }
+
+    /**
+     * Sever a link
+     * 
+     * @param links_
+     *            links of the current game
+     * @param nbConnections_
+     *            nodes connection counters
+     * @param linkIndex_
+     *            index of the link
+     */
+    public static void severLink(List<Link> links_, List<AtomicInteger> nbConnections_, int linkIndex_) {
+        links_.get(linkIndex_).sever();
+        nbConnections_.get(links_.get(linkIndex_).getNodeA()).decrementAndGet();
+        nbConnections_.get(links_.get(linkIndex_).getNodeB()).decrementAndGet();
+        links_.remove(linkIndex_);
+    }
 }
