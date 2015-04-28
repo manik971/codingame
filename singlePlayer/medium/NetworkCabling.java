dc234b8623f6b1e0dac1d5908975482b71b1f195
manik971 <campomanes.manuel@gmail.com>
2015-04-28 10:39:27 +0200

    create & update

---
+++ singlePlayer/medium/NetworkCabling.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Network Cabling
+ **/
+class Solution {
+
+    /**
+     * Main
+     */
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        // number of buildings
+        int nbBuildings = in.nextInt();
+        // initialize min & max building coordinates
+        long minX = in.nextInt();
+        long maxX = minX;
+        long[] coordY = new long[nbBuildings];
+        coordY[0] = in.nextInt();
+        // initialize all buildings coordinates
+        for (int i = 1; i < nbBuildings; i++) {
+            int X = in.nextInt();
+            int Y = in.nextInt();
+            coordY[i] = Y;
+            // update min & max coord
+            if (X < minX) {
+                minX = X;
+            }
+            else if (X > maxX) {
+                maxX = X;
+            }
+        }
+        // find the best main cable Y position
+        long moyY = getMedian(coordY);
+        // add main cable length
+        long cableLength = maxX - minX;
+        // add secondary cable length
+        for (int i = 0; i < coordY.length; i++) {
+            cableLength += Math.abs(moyY - coordY[i]);
+        }
+        System.out.println(cableLength);
+    }
+
+    /**
+     * Calculate the median of a list of values
+     * 
+     * @param coordY_
+     * @return the median value
+     */
+    public static long getMedian(long[] coordY_) {
+        long moyY;
+        Arrays.sort(coordY_);
+        // even
+        if (coordY_.length % 2 == 0) {
+            moyY = (long) Math.floor((coordY_[coordY_.length / 2] + coordY_[coordY_.length / 2 - 1]) / 2);
+        }
+        // odd
+        else {
+            moyY = coordY_[(int) Math.floor(coordY_.length / 2)];
+        }
+        return moyY;
+    }
+}
